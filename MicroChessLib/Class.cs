8b10f13308903a304f45b7037e0b6e45a146b625
Jacob Fliss <newgmobile@gmail.com>
2022-02-20 13:57:24 -0500

    fixed board, started PotentialForCheck method

--- MicroChessLib/Class.cs
+++ MicroChessLib/Class.cs
@@ -21,7 +21,7 @@ public struct Pieces
 
 public struct History
 {
-    public int PieceID { get; set; }
+    public Pieces Piece { get; set; }
     public Point OldPosition { get; set; }
     public Point NewPosition { get; set; }
     public DateTime StartTime { get; set; }
@@ -33,7 +33,7 @@ public class MCL
     public List<Pieces> pieces = new List<Pieces>();
     public List<History> history = new List<History>();
     public char[] letters = new char[] { ' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h' };
-    public string[] types = new string[] { "pawn", "rook", "knight", "bishop", "queen", "King" };
+    public string[] types = new string[] { "pawn", "rook", "knight", "bishop", "Queen", "King" };
     public int player = 0; // keep track of whos turn it is. 0 = white, bottom of screen
     public DateTime MoveStartTime = DateTime.Now;
 
@@ -52,23 +52,55 @@ public class MCL
         {
             if (PieceToCheck.Y == (YDiff != 0 ? EndPosition.Y < 0 ? StartPosition.Y - i : StartPosition.Y + i : StartPosition.Y) &&
                 PieceToCheck.X == (XDiff != 0 ? EndPosition.X < 0 ? StartPosition.X - i : StartPosition.X + i : StartPosition.X))
+            {
+                Console.WriteLine("Collision detected at {0}{1}", letters[PieceToCheck.X], PieceToCheck.Y);
                 return true;
+            }
         }
         return false;
     }
 
-    public bool AttackOtherPiece(Pieces p) // untested
+    // TO-DO: finish and test
+    public bool PotentialForCheck() // check if either king would be in check
+    {
+        if (history.Count <= 0) return false; // no one moved yet
+        foreach (Pieces p in pieces.FindAll(o => !o.dead)) // check each piece and every space
+        {
+            int row = 0;
+            for (int i = 1; i <= 64; i++)
+            {
+                int attack = 0;
+                if (MoveValidity(p, new Point { X = i - (8 * row), Y = row + 1 }, out attack)) // check if potential move is valid, would be an attack and on a king
+                {
+                    if (attack > 0)
+                    {
+                        Pieces tmp = pieces.Find(o => o.id == attack);
+                        if (tmp.type == 5 && tmp.side == player)
+                        { // we're about to put ourselves in check
+                            Console.WriteLine("Potential for check from {2} at {0}{1}", tmp.coords.X, tmp.coords.Y, types[tmp.type]);
+                            return true;
+                        }
+                    }
+                }
+                if (i % 8 == 0) { row++; }
+            }
+        }
+        return false;
+    }
+
+    // TO-DO: test
+    public bool AttackOtherPiece(Pieces MyPiece)
     {
         foreach (Pieces tmp in pieces)
         {
-            if (!tmp.dead && tmp.side != player && tmp.coords.X == p.coords.X && tmp.coords.Y == p.coords.Y) // check if landing on another piece
+            if (!tmp.dead && tmp.side != player && tmp.coords.X == MyPiece.coords.X && tmp.coords.Y == MyPiece.coords.Y) // check if landing on another piece
             {
-                Pieces temp = pieces.Find(tmp => tmp.id == p.id);
+                Pieces temp = pieces.Find(tmp => tmp.id == MyPiece.id);
                 temp.dead = true;
                 pieces[tmp.id] = temp;
                 if (temp.type == 5)
                 {
-                    Console.WriteLine("The king has died. {0} won! Game over.", player == 0 ? "white" : "black");
+                    Console.WriteLine("The king has died. {0} side won! Game over.", player == 0 ? "white" : "black");
                 }
                 return true; // should only land on 1 piece
             }
@@ -76,47 +108,45 @@ public class MCL
         return false;
     }
 
-    public bool MovePiece(int id, Point coords)
+    public bool MoveValidity(Pieces p, Point coords, out int AttackID)
     {
-        Pieces p = pieces.Find(tmp => tmp.id == id);
-        Console.WriteLine("Moving \"{2}\"[{0}] piece from {3} to {1}...", id, coords, types[p.type], p.coords);
         bool valid = false;
-        History NewHistory = new History() { PieceID = id, StartTime = MoveStartTime, EndTime = DateTime.Now, OldPosition = p.coords, NewPosition = coords };
+        AttackID = 0;
+        History NewHistory = new History() { Piece = p, StartTime = MoveStartTime, EndTime = DateTime.Now, OldPosition = p.coords, NewPosition = coords };
 
         if (coords.X < 1 || coords.X > 8 || coords.Y < 1 || coords.Y > 8) // dont go beyond board
-            return false;
-        if (pieces.FindIndex(piece => piece.type == 5 && piece.dead) > 0) // king is dead
             return false;
 
         switch (p.type) // check if move is valid before other checks
         {
-            case 0: // pawn - only y axis, attacks are diagonal
-                if ((player == 0 && coords.Y > p.coords.Y && coords.X == p.coords.X) || (player == 1 && coords.Y < p.coords.Y && coords.X == p.coords.X))
+            case 0: // pawn - only y axis 1 place, first move can be 2 places, attacks are diagonal
+                if ((player == 0 && p.coords.Y <= coords.Y + (history.FindIndex(o => o.Piece.id == p.id) > 0 ? 1 : 2) && coords.X == p.coords.X) ||
+                    (player == 1 && p.coords.Y >= coords.Y - (history.FindIndex(o => o.Piece.id == p.id) > 0 ? 1 : 2) && coords.X == p.coords.X))
                     valid = true;
-                if (((coords.X == (p.coords.X - 1) && coords.Y == (p.coords.Y - 1)) && player == 0 && player != p.side) ||
-                    ((coords.X == (p.coords.X + 1) && coords.Y == (p.coords.Y + 1)) && player == 0 && player != p.side) ||
-                    ((coords.X == (p.coords.X - 1) && coords.Y == (p.coords.Y + 1)) && player == 1 && player != p.side) ||
-                    ((coords.X == (p.coords.X + 1) && coords.Y == (p.coords.Y - 1)) && player == 1 && player != p.side))
-                {
-                    if (AttackOtherPiece(p)) { history.Add(NewHistory); return true; }
+                if (((coords.X == (p.coords.X - 1) && coords.Y == (p.coords.Y - 1)) && player == 0 && pieces.FindIndex(o => o.coords.Equals(coords) && o.side != player) > 0) ||
+                    ((coords.X == (p.coords.X + 1) && coords.Y == (p.coords.Y + 1)) && player == 0 && pieces.FindIndex(o => o.coords.Equals(coords) && o.side != player) > 0) ||
+                    ((coords.X == (p.coords.X - 1) && coords.Y == (p.coords.Y + 1)) && player == 1 && pieces.FindIndex(o => o.coords.Equals(coords) && o.side != player) > 0) ||
+                    ((coords.X == (p.coords.X + 1) && coords.Y == (p.coords.Y - 1)) && player == 1 && pieces.FindIndex(o => o.coords.Equals(coords) && o.side != player) > 0))
+                { // attack
+                    AttackID = pieces.Find(o => o.coords.Equals(coords)).id;
+                    return true;
                 }
                 break;
             case 1: // rooks - straight lines
                 if ((coords.X == p.coords.X && coords.Y != p.coords.Y) || (coords.X != p.coords.X && coords.Y == p.coords.Y))
                     valid = true;
                 break;
-            case 2: // knights - L shape
+            case 2: // knights - L shape, ignores path collisions
                 if ((coords.X - p.coords.X == 2 && coords.Y - p.coords.Y == 1) || (coords.X - p.coords.X == 1 && coords.Y - p.coords.Y == 2))
                 {
-                    if (AttackOtherPiece(p)) { history.Add(NewHistory); return true; }
-                    else
+                    foreach (Pieces tmp in pieces)
                     {
-                        foreach (Pieces tmp in pieces)
-                        {
-                            if (!tmp.dead && tmp.side == player && tmp.coords.X == p.coords.X && tmp.coords.Y == p.coords.Y) // check if landing on another piece
-                                return false; // can't land on our own piece
-                        }
-                    } // MUST RETURN, can't do collision detection
+                        if (!tmp.dead && tmp.side == player && tmp.coords.X == p.coords.X && tmp.coords.Y == p.coords.Y) // check if landing on another piece
+                            return false; // can't land on our own piece
+                        else if (!tmp.dead && tmp.side != player && tmp.coords.X == p.coords.X && tmp.coords.Y == p.coords.Y)
+                            AttackID = pieces.Find(o => o.coords.Equals(coords)).id;
+                    }
+                    return true; // ignore path collisions
                 }
                 break;
             case 3: // bishops - diagonals only
@@ -137,15 +167,41 @@ public class MCL
                 break;
         }
 
-        if (valid) // check for collisions, except knights, only if piece move was valid
+        if (valid && p.type != 2) // check for collisions, except knights, only if piece move was valid
         {
             foreach (Pieces tmp in pieces)
             {
-                if (p.type == 2 || p.dead || p.id == tmp.id) continue;
+                if (tmp.dead || tmp.id == p.id) continue; // ignore our piece and dead pieces
                 if (DetectCollisionPath(new Point() { X = p.coords.X, Y = p.coords.Y }, new Point() { X = coords.X, Y = coords.Y }, new Point() { X = tmp.coords.X, Y = tmp.coords.Y }))
-                    return false;
+                {
+                    if (tmp.coords.Equals(coords) && tmp.side != player)
+                    { // move was valid, technically we collided, but our final path was on top of other player's piece, attack it
+                        AttackID = pieces.Find(o => o.coords.Equals(coords)).id;
+                        return true;
+                    }
+                    return false; // we collided with a piece on our way to our destination
+                }
             }
-            history.Add(NewHistory);
+            return true;
+        }
+        return false;
+    }
+
+    public bool MovePiece(Pieces p, Point coords)
+    {
+        Console.WriteLine("Moving \"{2}\"[{0}] piece from {3} to {1}...", p.id, String.Concat(letters[coords.X], coords.Y), types[p.type], String.Concat(letters[p.coords.X], p.coords.Y));
+        
+        if (pieces.FindIndex(piece => piece.type == 5 && piece.dead) > 0) // king is dead
+            return false;
+
+        int IsAttack = 0; // piece ID we are attacking, if greater than 0
+        if (MoveValidity(p, coords, out IsAttack)) { // validate we can move in space
+            if (PotentialForCheck()) return false; // we can move there, but we would check ourselves
+            if (IsAttack > 0) {
+                if (AttackOtherPiece(p)) {
+                    Pieces Att = pieces.Find(o => o.id == IsAttack); Console.WriteLine("Attacking {0} at {0}{1}", types[Att.type], Att.coords.X, Att.coords.Y);
+                } } // attack other pieces if we happen to land on them
+            history.Add(new History() { Piece = p, StartTime = MoveStartTime, EndTime = DateTime.Now, OldPosition = p.coords, NewPosition = coords }); // success, add move history
             return true;
         }
         return false;
--- MicroChessTest/Program.cs
+++ MicroChessTest/Program.cs
@@ -15,7 +15,7 @@ class Chess
         m.SetupBoard();
         while (true)
         {
-            Console.Write("cmd: ");
+            Console.Write("[{0}] cmd: ", m.player == 0 ? "white" : "black");
             string val = Console.ReadLine() ?? "";
             if (val.Equals("help")) Console.WriteLine("RESET - Place pieces back to original places{0}MOVE - Move piece by ID to coordinates.{0}BOARD - Show game board{0}PIECES - List all pieces on board", System.Environment.NewLine);
             else if (val.Equals("reset")) m.SetupBoard();
@@ -23,7 +23,7 @@ class Chess
             {
                 char test = Convert.ToChar(val.Split(" ")[2][0].ToString().ToLower());
                 int found = Array.IndexOf(m.letters, test);
-                if (!m.MovePiece(Convert.ToInt32(val.Split(" ")[1]), new Point() { X = found, Y = val.Split(" ")[2][1] - '0' }))
+                if (!m.MovePiece(m.pieces.Find(o => o.id == Convert.ToInt32(val.Split(" ")[1])), new Point() { X = found, Y = val.Split(" ")[2][1] - '0' }))
                     Console.WriteLine("move was invalid");
                 else { m.player = (m.player + 1) % 2; Console.WriteLine("Move was successful at {1}. {0} side's turn.", m.player == 0 ? "white" : "black", DateTime.Now); }
             }
@@ -35,22 +35,26 @@ class Chess
 
     void ShowBoard()
     {
-        int row = 0;
-        Console.Write("  A B C D E F G H\n  ---------------\n1");
-        for (int i = 1; i <= 64; i++)
+        Console.Write("   A   B   C   D   E   F   G   H\n  -------------------------------\n");
+        for (int r = 8; r >= 1; r--)
         {
-            Pieces p = m.pieces.Find(tmp => tmp.coords.Equals(new Point { X = i - (8 * row), Y = row + 1 }));
-            Console.Write("|{0}", p.id <= 0 ? " " : m.types[p.type][0] );
-            if (i % 8 == 0) { row++; Console.Write("|{0}  ---------------{0}", System.Environment.NewLine); if (i != 64) Console.Write("{0}", i / 8 + 1); }
+            string BoardLine = r.ToString();
+            for (int s = (r * 8); s > ((r * 8) - 8); s--)
+            {
+                Pieces p = m.pieces.Find(tmp => tmp.coords.Equals(new Point { X = ((8 * r) - s) + 1, Y = r }));
+                if (p.id > 0) BoardLine += ("|" + String.Concat(m.types[p.type][0], p.id.ToString("D2"))); else BoardLine += "|   ";
+            }
+
+            Console.Write("{0}|{1}\n  -------------------------------\n", BoardLine, r);
         }
-        Console.WriteLine("");
+        Console.WriteLine("   A   B   C   D   E   F   G   H\n");
     }
 
     void ShowPiecesList()
     {
         foreach (Pieces p in m.pieces)
         {
-            Console.WriteLine("{0}-{1}[{2}]:{3}", p.side == 0 ? "White" : "Black", m.types[p.type], p.id, String.Concat(m.letters[p.coords.X], p.coords.Y));
+            Console.WriteLine("[{0}] {1}[{2}]:{3}", p.side == 0 ? "W" : "B", m.types[p.type], p.id, String.Concat(m.letters[p.coords.X], p.coords.Y));
         }
     }
 
@@ -58,8 +62,7 @@ class Chess
     {
         foreach (History h in m.history)
         {
-            Pieces p = m.pieces.Find(tmp => tmp.id == h.PieceID);
-            Console.WriteLine("[{5}] {0} {1}[{2}] from {3} to {4} - Duration: {6}", p.side == 0 ? "White" : "Black", m.types[p.type], p.id, String.Concat(m.letters[h.OldPosition.X], h.OldPosition.Y), String.Concat(m.letters[h.NewPosition.X], h.NewPosition.Y), h.EndTime, h.StartTime - h.EndTime);
+            Console.WriteLine("[{5}] {0} {1}[{2}] from {3} to {4} - Duration: {6}", h.Piece.side == 0 ? "White" : "Black", m.types[h.Piece.type], h.Piece.id, String.Concat(m.letters[h.OldPosition.X], h.OldPosition.Y), String.Concat(m.letters[h.NewPosition.X], h.NewPosition.Y), h.EndTime, h.StartTime - h.EndTime);
         }
     }
 }
